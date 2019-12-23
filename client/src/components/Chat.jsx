e47156d278c7ce4aeb2fa92d7cc57df5339563f4
iteremasov <teremasovjr@gmail.com>
2019-12-23 22:48:49 +0600

    Chat: Send on Enter

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './chat.css';
 
 export default class Chat extends Component {
-	state = { post: '' };
+	state = { post: '', Shift: false };
 
 	changePost = e => {
 		this.setState({ post: e.target.value });
@@ -14,6 +14,22 @@ export default class Chat extends Component {
 		const data = { user: user, post: this.state.post };
 		setPost(data);
 		this.setState({ post: '' });
+	};
+
+	checkKeyKode = event => {
+		if (event.key === 'Shift') {
+			this.setState({ Shift: true });
+		}
+		if (event.key === 'Enter' && !this.state.Shift) {
+			event.preventDefault();
+			this.click();
+		}
+	};
+
+	cleanShift = event => {
+		if (event.key === 'Shift') {
+			this.setState({ Shift: false });
+		}
 	};
 	render() {
 		const { posts, user } = this.props;
@@ -41,7 +57,13 @@ export default class Chat extends Component {
 					})}
 				</div>
 				<div>
-					<textarea value={this.state.post} onChange={this.changePost} className="input-messege"></textarea>
+					<textarea
+						onKeyDown={this.checkKeyKode}
+						onKeyUp={this.cleanShift}
+						value={this.state.post}
+						onChange={this.changePost}
+						className="input-messege"
+					></textarea>
 				</div>
 				<button onClick={this.click} className="chat-button">
 					send
