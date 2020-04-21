8c68d2019908f7c91deabf2ce10147b342b6cce2
vteremasov <vteremasov@klika-tech.com>
2020-04-21 21:37:01 +0300

    Add prod build configs

---
+++ server/build-prod.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+IMAGE_NAME=pplanning-server:latest
+
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=${IMAGE_NAME} --format="{{.ID}}"))
+
+docker build -t $IMAGE_NAME .
+
+docker run -d -p 8081:80 $IMAGE_NAME
