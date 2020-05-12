74cd6559823340b354ebc2e082cf7137713f43ca
iteremasov <teremasovjr@gmail.com>
2020-05-12 23:31:03 +0600

    refaktoring UserName (functional component)

--- client/src/components/UserName.jsx
+++ client/src/components/UserName.jsx
@@ -1,65 +1,86 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import PlaningRoom from '../pages/PlanningRoom';
-
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import { withRouter } from "react-router";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
-import './userName.css';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    justifyContent: 'center',
+  },
+}))
 
-export default class UserName extends Component {
-	state = { textFieldValue: '', userName: '' };
+function UserName(props) {
+  const classes = useStyles();
+  const [userName, setUserName] = useState('');
+  const [textFieldValue, setTextFieldValue] = useState('')
 
-	click = () => {
-		this.setState({ userName: this.state.textFieldValue });
-	};
+  const click = () => {
+    setUserName(textFieldValue);
+  };
 
-	checkKey = event => {
-		if(event.key === 'Enter'){
-			event.preventDefault();
-			this.click();
-		}
-	}
-	handleTextFieldChange = e => {
-		this.setState({
-			textFieldValue: e.target.value,
-		});
-	};
+  const checkKey = event => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      click();
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				{this.state.userName ? (
-					<PlaningRoom userName={this.state.userName} roomkey={this.props.match.params.roomkey} />
-				) : (
-					<div className="content">
-						<h2>Enter your name. This will help your colleagues identify you.</h2>
-						<form action="" onSubmit={this.click}>
-						<div className="inputUserName">
-							<TextField
-								autoFocus
-								onKeyDown={this.checkKey}
-								onChange={this.handleTextFieldChange}
-								id="outlined-name-input"
-								label="name"
-								className="inputName"
-								margin="dense"
-								variant="outlined"
-							/>
-							<Button
-								size="large"
-								disabled={this.state.textFieldValue ? false : true}
-								type="submit"
-								className="button"
-							>
-								Join planning
+  const handleTextFieldChange = e => {
+    setTextFieldValue(e.target.value);
+  };
+
+
+
+  return (
+    <>
+      {
+        userName ?
+          (
+            <PlaningRoom userName={userName} roomkey={props.match.params.roomkey} />
+          ) : (
+            <Card>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Enter your name. This will help your colleagues identify you.
+              </Typography>
+                <CardActions className={classes.root}>
+                  <form action="" onSubmit={click}>
+                    <div className="inputUserName">
+                      <TextField
+                        autoFocus
+                        onKeyDown={checkKey}
+                        onChange={handleTextFieldChange}
+                        id="outlined-name-input"
+                        label="name"
+                        className="inputName"
+                        margin="dense"
+                        variant="outlined"
+                      />
+                      <Button
+                        size="large"
+                        disabled={textFieldValue ? false : true}
+                        type="submit"
+                        className="button"
+                      >
+                        Join planning
 							</Button>
-						</div>
-						</form>
+                    </div>
+                  </form>
+                </CardActions>
+              </CardContent>
+            </Card>
+          )
 
-					</div>
-				)}
-			</div>
-		);
-	}
+      }
+    </>
+  )
 }
+
+export default withRouter(UserName);
