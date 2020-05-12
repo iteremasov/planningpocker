2fca335e8a70318ff3103b1815b155260f98cbb2
iteremasov <teremasovjr@gmail.com>
2020-05-12 22:51:18 +0600

    refaktoring StartPlanning (ffunctional component)

--- client/src/components/StartPlanning.jsx
+++ client/src/components/StartPlanning.jsx
@@ -1,43 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { fetchJsonPost } from '../Services/basicServices';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
-import './startPlanning.css';
 import Button from '@material-ui/core/Button'
+import { withRouter } from "react-router";
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class StartPlanning extends Component {
-  makeRoom = () => {
-    fetchJsonPost(process.env.REACT_APP_URI_HTTP + 'rooms', {})
-      .then(response => response.json())
-      .then(response => this.props.history.push('/' + response.id))
-      .catch(err => console.log(err));
-  };
-  render() {
-    const styles = {
-      root: {
-        justifyContent: 'center'
-      }
-    };
-    return (
-      <Card>
-        <CardContent>
-          <Typography variant="h3" component="h2">
-            planning-pocker service
+const useStyles = makeStyles((theme) => ({
+  root: {
+    justifyContent: 'center',
+  },
+}))
+
+const makeRoom = (props) => {
+  fetchJsonPost(process.env.REACT_APP_URI_HTTP + 'rooms', {})
+    .then(response => response.json())
+    .then(response => props.history.push('/' + response.id))
+    .catch(err => console.log(err));
+};
+
+
+function StartPlanning(props) {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h3" component="h2">
+          planning-pocker service
           </Typography>
-          <Typography variant="h5" component="h2">
-            Press the button to start
+        <Typography variant="h5" component="h2">
+          Press the button to start
           </Typography>
-          <CardActions classes={{root: styles.root}}>
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={this.makeRoom}>Create <br /> Room
+        <CardActions className={classes.root} >
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => { makeRoom(props) }}>Create <br /> Room
              </Button>
-          </CardActions>
-        </CardContent>
-      </Card>
-    );
-  }
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
 }
+
+export default withRouter(StartPlanning)
