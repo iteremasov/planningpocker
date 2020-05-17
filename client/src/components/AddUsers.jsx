e8fe29766fc573ecca39c7acf58edd0561085049
iteremasov <teremasovjr@gmail.com>
2020-05-18 00:58:51 +0600

    auto select url in a addUser component

--- client/src/components/AddUsers.jsx
+++ client/src/components/AddUsers.jsx
@@ -1,60 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const useStyles = makeStyles((theme) => ({
-  addIcon: {
-    color: "white",
-  },
-  textfield: {
-    marginLeft: "1px"
-  },
-  copyIcon: {
-    marginTop: "3%",
-    marginRight: "1px"
+export default class AddUsers extends Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.selectText = this.selectText.bind(this);
   }
-}))
+  componentDidUpdate = () =>{
+    if (this.state.anchorEl){
+      this.selectText()
+    }
+  }
+  selectText() {
+    this.textInput.current.select();
+  }
+  state = {
+    anchorEl: null
+  }
 
-export default function AddUsers({ url }) {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  setAnchorEl = (e) => {
+    this.setState({ anchorEl: e })
+  }
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  handleClick = (event) => {
+    this.setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  handleClose = () => {
+    this.setAnchorEl(null);
   };
 
-  return (
-    <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        <PersonAddIcon className={classes.addIcon} />
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <Typography variant="h5" component="h2">
-          Share this Link
+  render() {
+    const url = this.props.url
+    return (
+      <div>
+        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
+          <PersonAddIcon />
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={this.state.anchorEl}
+          keepMounted
+          open={Boolean(this.state.anchorEl)}
+          onClose={this.handleClose}
+        >
+          <Typography variant="h5" component="h2">
+            Share this Link
         </Typography>
-        <TextField className={classes.textfield} variant="outlined" margin="dense" defaultValue={url} disabled={true} size='small' />
-        <CopyToClipboard text={url}>
-          <Button className={classes.copyIcon}>
-            <FileCopyIcon />
-          </Button>
-        </CopyToClipboard>
-      </Menu>
-    </div>
-  );
+          <TextField inputRef={this.textInput} variant="outlined" margin="dense" value={url} size='small' />
+          <CopyToClipboard text={url}>
+            <Button onClick={this.selectText} >
+              <FileCopyIcon />
+            </Button>
+          </CopyToClipboard>
+        </Menu>
+      </div>
+    )
+  }
 }
